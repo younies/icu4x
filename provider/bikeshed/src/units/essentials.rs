eadc1062bdadadf573d8a38aefbef7e04929de01
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-06-30 15:14:44 +0200

    Fill powers of the essential data
    Summary:
    
    Test Plan:

--- provider/bikeshed/src/units/essentials.rs
+++ provider/bikeshed/src/units/essentials.rs
@@ -2,13 +2,16 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 
+use crate::cldr_serde::units::data::Patterns;
 use crate::cldr_serde::{self};
 use crate::DatagenProvider;
 
 use icu::experimental::dimension::provider::units_essentials::UnitsEssentialsV1Marker;
 
+use icu::experimental::dimension::provider::pattern_key::{PatternKey, PowerValue};
+use icu::experimental::dimension::provider::units_essentials::CompoundCount;
 use icu::experimental::dimension::provider::units_essentials::UnitsEssentialsV1;
 use icu_provider::prelude::*;
 use icu_provider::DataMarkerAttributes;
@@ -42,13 +45,80 @@ impl DataProvider<UnitsEssentialsV1Marker> for DatagenProvider {
             .ok_or_else(|| DataError::custom("Failed to get times"))?
             .clone();
 
-        // TODO: Fill prefixes (power, si prefixes, ... etc.) in the next PR.
-        let prefixes = ZeroMap::new();
+        // TODO: Fill prefixes (si prefixes, ... etc.) in the next PR.
+        let mut prefixes = BTreeMap::<PatternKey, String>::new();
+        fn fill_power(
+            prefixes: &mut BTreeMap<PatternKey, String>,
+            powers: &Patterns,
+            power_value: PowerValue,
+        ) {
+            let counts = [
+                (
+                    powers.zero_compound_unit_pattern1.as_ref(),
+                    CompoundCount::Zero,
+                ),
+                (
+                    powers.one_compound_unit_pattern1.as_ref(),
+                    CompoundCount::One,
+                ),
+                (
+                    powers.two_compound_unit_pattern1.as_ref(),
+                    CompoundCount::Two,
+                ),
+                (
+                    powers.few_compound_unit_pattern1.as_ref(),
+                    CompoundCount::Few,
+                ),
+                (
+                    powers.many_compound_unit_pattern1.as_ref(),
+                    CompoundCount::Many,
+                ),
+                (
+                    powers.other_compound_unit_pattern1.as_ref(),
+                    CompoundCount::Other,
+                ),
+            ];
+
+            let counts = counts
+                .iter()
+                .filter(|(pattern, _)| pattern.is_some())
+                .map(|(pattern, count)| (pattern.unwrap(), count));
+
+            for (pattern, count) in counts {
+                prefixes.insert(
+                    PatternKey::Power {
+                        power: power_value,
+                        count: *count,
+                    },
+                    pattern.to_string(),
+                );
+            }
+        }
+
+        fn fill_powers(
+            prefixes: &mut BTreeMap<PatternKey, String>,
+            length_data: &BTreeMap<String, Patterns>,
+        ) -> Result<(), DataError> {
+            for (powers, power_value) in [
+                (length_data.get("power2"), PowerValue::Two),
+                (length_data.get("power3"), PowerValue::Three),
+            ] {
+                fill_power(
+                    prefixes,
+                    powers.ok_or_else(|| DataError::custom("Failed to get powers"))?,
+                    power_value,
+                );
+            }
+
+            Ok(())
+        }
+
+        fill_powers(&mut prefixes, length_data)?;
 
         let result = UnitsEssentialsV1 {
             per: per.into(),
             times: times.into(),
-            prefixes,
+            prefixes: ZeroMap::from_iter(prefixes),
         };
 
         Ok(DataResponse {
