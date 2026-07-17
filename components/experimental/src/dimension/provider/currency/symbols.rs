4641d79d3a4f78f16971455eb3e4d236097ff60a
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-07-17 12:49:00 +0100

    Clean up experimental data (#8236)
    Based on https://github.com/unicode-org/icu4x/pull/8235
    
    Remove stale data, use `MaybeAsVarULE` to reduce baked size, flatten
    structs to improve JSON size.
    
    ## Changelog
    
    `icu_experimental`
    * Remove `UnitsFormatter`, use `CategorizedUnitsFormatter`

--- components/experimental/src/dimension/provider/currency/symbols.rs
+++ components/experimental/src/dimension/provider/currency/symbols.rs
@@ -12,15 +12,6 @@ use zerovec::VarZeroCow;
 use zerovec::ule::vartuple::{VarTuple, VarTupleULE};
 
 use crate::dimension::currency::CurrencyCode;
-#[cfg(feature = "compiled_data")]
-/// Baked data
-///
-/// <div class="stab unstable">
-/// 🚧 This code is considered unstable; it may change at any time, in breaking or non-breaking ways,
-/// including in SemVer minor releases. In particular, the `DataProvider` implementations are only
-/// guaranteed to match with this version's `*_unstable` providers. Use with caution.
-/// </div>
-pub use crate::provider::Baked;
 
 icu_provider::data_marker!(
     /// Currency symbol data needed for short and narrow currency formatting.
@@ -81,4 +72,15 @@ impl CurrencySymbolsV1 {
     }
 }
 
-icu_provider::data_struct!(CurrencySymbol<'_>, #[cfg(feature = "datagen")]);
+icu_provider::data_struct!(
+    CurrencySymbol<'_>,
+    varule: VarTupleULE<u8, str>,
+    #[cfg(feature = "datagen")]
+    encode_as_varule: |v: &CurrencySymbol<'_>| &v.0
+);
+
+impl<'zf> zerofrom::ZeroFrom<'zf, VarTupleULE<u8, str>> for CurrencySymbol<'zf> {
+    fn zero_from(source: &'zf VarTupleULE<u8, str>) -> Self {
+        Self(VarZeroCow::zero_from(source))
+    }
+}
